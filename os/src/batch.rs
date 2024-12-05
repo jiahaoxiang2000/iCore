9b021ad9e072df5f0de3c8057ffc0a9b1c2258ee
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-12-05 17:07:45 +0800

    see batch os

--- os/src/batch.rs
+++ os/src/batch.rs
@@ -73,14 +73,20 @@ impl AppManager {
             shutdown(false);
         }
         println!("[kernel] Loading app_{}", app_id);
-        // clear app area
-        core::slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, APP_SIZE_LIMIT).fill(0);
-        let app_src = core::slice::from_raw_parts(
-            self.app_start[app_id] as *const u8,
-            self.app_start[app_id + 1] - self.app_start[app_id],
-        );
-        let app_dst = core::slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, app_src.len());
-        app_dst.copy_from_slice(app_src);
+        
+        unsafe {
+            // clear app area
+            core::slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, APP_SIZE_LIMIT).fill(0);
+            // get the app code slice
+            let app_src = core::slice::from_raw_parts(
+                self.app_start[app_id] as *const u8,
+                self.app_start[app_id + 1] - self.app_start[app_id],
+            );
+            // target app code address on the APP_BASE_ADDRESS
+            let app_dst = core::slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, app_src.len());
+            // copy the app code to the target address
+            app_dst.copy_from_slice(app_src);
+        }
         // Memory fence about fetching the instruction memory
         // It is guaranteed that a subsequent instruction fetch must
         // observes all previous writes to the instruction memory.
