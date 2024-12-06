c2577890b2d3b31372e15b48e45f741c7345eff7
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-12-06 22:28:33 +0800

    ch4: is the user bin not to 7 application

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![feature(linkage)]
 
+
 #[macro_use]
 pub mod console;
 mod lang_items;
@@ -9,7 +10,6 @@ mod syscall;
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
-    clear_bss();
     exit(main());
     panic!("unreachable after sys_exit!");
 }
@@ -18,16 +18,6 @@ pub extern "C" fn _start() -> ! {
 #[no_mangle]
 fn main() -> i32 {
     panic!("Cannot find main!");
-}
-
-fn clear_bss() {
-    extern "C" {
-        fn start_bss();
-        fn end_bss();
-    }
-    (start_bss as usize..end_bss as usize).for_each(|addr| unsafe {
-        (addr as *mut u8).write_volatile(0);
-    });
 }
 
 use syscall::*;
@@ -43,4 +33,8 @@ pub fn yield_() -> isize {
 }
 pub fn get_time() -> isize {
     sys_get_time()
+}
+
+pub fn sbrk(size: i32) -> isize {
+    sys_sbrk(size)
 }
